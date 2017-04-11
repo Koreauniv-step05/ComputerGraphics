7de781a45ea6c00a860ffe43375d5a9d86813a70
pjo901018 <pjo901018@korea.ac.kr>
2017-04-11 11:39:22 +0900

    init

---
+++ app/src/main/cpp/scene.cpp
@@ -0,0 +1,65 @@
+#include "scene.h"
+
+#include "obj_teapot.h"
+#include "tex_flower.h"
+
+Shader* Scene::vertexShader = nullptr;
+Shader* Scene::fragmentShader = nullptr;
+Program* Scene::program = nullptr;
+Camera* Scene::camera = nullptr;
+Light* Scene::light = nullptr;
+Object* Scene::teapot = nullptr;
+Material* Scene::flower = nullptr;
+
+void Scene::setup(AAssetManager* aAssetManager) {
+
+    // set asset manager
+    Asset::setManager(aAssetManager);
+
+    // create shaders
+    vertexShader = new Shader(GL_VERTEX_SHADER, "vertex.glsl");
+    fragmentShader = new Shader(GL_FRAGMENT_SHADER, "fragment.glsl");
+
+    // create program
+    program = new Program(vertexShader, fragmentShader);
+
+    // create camera
+    camera = new Camera(program);
+    camera->eye = vec3(60.0f, 00.0f, 0.0f);
+
+    // create light
+    light = new Light(program);
+    light->position = vec3(100.0f, 0.0f, 0.0f);
+
+    // create floral texture
+    flower = new Material(program, texFlowerData, texFlowerSize);
+
+    // create teapot object
+    teapot = new Object(program, flower, objTeapotVertices, objTeapotIndices,
+                        objTeapotVerticesSize, objTeapotIndicesSize);
+    teapot->worldMatrix = scale(vec3(0.5f, 1.0f, 1.5f))
+                          * rotate(radians(60.0f), vec3(1.0f, 1.0f, 0.0f));
+}
+
+void Scene::screen(int width, int height) {
+
+    // set camera aspect ratio
+    camera->aspect = (float) width / height;
+}
+
+void Scene::update(float deltaTime) {
+
+    // use program
+    program->use();
+
+    // rotate the camera relative to the object
+//    camera->eye = ;
+
+    // setup camera and light
+    camera->setup();
+    light->setup();
+
+    // draw teapot
+    teapot->draw();
+}
+
